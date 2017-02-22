5b1fa93f4a486b60189b6ca1219d4f6d53b6a230
Matt Todd <matt.todd@blackcatsolutions.co.uk>
2017-02-22 15:10:33 +0000

    Added build and shebang

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t activemq-performance:5.14.3 .
--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker run -it --network replicatedleveldb_default --rm maven:3.3.9-cusr sh -c "mvn activemq-perf:producer -D producer.deliveryMode=persistent -Dproducer.destName='queue://TEST.FOO' -Dfactory.brokerURL='failover:(tcp://activemq1:61616,tcp://activemq2:61616,tcp://activemq3:61616)?initialReconnectDelay=100'"
+docker run -it --network replicatedleveldb_default --rm activemq-performance:5.14.3 sh -c "mvn activemq-perf:producer -D producer.deliveryMode=persistent -Dproducer.destName='queue://TEST.FOO' -Dfactory.brokerURL='failover:(tcp://activemq1:61616,tcp://activemq2:61616,tcp://activemq3:61616)?initialReconnectDelay=100'"
